a75049e3b3b3b69cafa904569ddf0466ca84c6a9
lucky masharane <lmasharane@gmail.com>
2021-01-25 13:45:58 +0200

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCqUXmcuulZ67eID4HleFo7odHgaGqOMmc",
+    authDomain: "weather-app-bbe4c.firebaseapp.com",
+    projectId: "weather-app-bbe4c",
+    storageBucket: "weather-app-bbe4c.appspot.com",
+    messagingSenderId: "1052834113735",
+    appId: "1:1052834113735:web:7f14dae26d90a83c71f6e2",
+    API_Key: '82f5923abcad8efed20836722c840609',
+    API_URL: 'http://api.openweathermap.org/data/2.5'
+  }
 };
